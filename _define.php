465651b1ccb2c50130f01e6439fab8dbabbaa8b8
franck <carnet.franck.paul@gmail.com>
2013-09-17 16:31:27 +0200

    Next release will be 0.3

--- _define.php
+++ _define.php
@@ -3,7 +3,7 @@
 #
 # This file is part of Dotclear 2.
 #
-# Copyright (c) 2003-2011 Franck Paul
+# Copyright (c) 2003-2013 Franck Paul
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -15,7 +15,7 @@ $this->registerModule(
 	/* Name */			"User defined thumbnails",
 	/* Description*/		"Add user defined thumbnails",
 	/* Author */			"Franck Paul",
-	/* Version */			'0.2',
+	/* Version */			'0.3',
 	array(
 		'permissions' =>	'usage,contentadmin'
 	)
