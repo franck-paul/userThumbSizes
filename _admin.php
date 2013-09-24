17fb7610017a6fae0dd9de2aa391a0aa32b50490
franck <carnet.franck.paul@gmail.com>
2013-09-24 13:37:51 +0200

    locales

--- _admin.php
+++ _admin.php
@@ -14,6 +14,9 @@
 
 if (!defined('DC_CONTEXT_ADMIN')) { return; }
 
+// dead but useful code, in order to have translations
+__('User defined thumbnails').__('Add user defined thumbnails');
+
 $_menu['Blog']->addItem(__('User defined thumbnails'),'plugin.php?p=userThumbSizes','index.php?pf=userThumbSizes/icon.png',
 		preg_match('/plugin.php\?p=userThumbSizes(&.*)?$/',$_SERVER['REQUEST_URI']),
 		$core->auth->check('contentadmin',$core->blog->id));
