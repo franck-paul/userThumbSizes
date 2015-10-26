5a3f4d8bf61e4aba454894da4a5d5820c38dd47a
franck <carnet.franck.paul@gmail.com>
2015-10-26 17:16:20 +0100

    Using dcPage::cssLoad, dcPage::jsLoad and dcPage::getPF is a better way to load plugin resources

--- _admin.php
+++ _admin.php
@@ -17,6 +17,8 @@ if (!defined('DC_CONTEXT_ADMIN')) { return; }
 // dead but useful code, in order to have translations
 __('User defined thumbnails').__('Add user defined thumbnails');
 
-$_menu['Blog']->addItem(__('User defined thumbnails'),'plugin.php?p=userThumbSizes','index.php?pf=userThumbSizes/icon.png',
+$_menu['Blog']->addItem(__('User defined thumbnails'),
+		'plugin.php?p=userThumbSizes',
+		urldecode(dcPage::getPF('userThumbSizes/icon.png')),
 		preg_match('/plugin.php\?p=userThumbSizes(&.*)?$/',$_SERVER['REQUEST_URI']),
 		$core->auth->check('contentadmin',$core->blog->id));
