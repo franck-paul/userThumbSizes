72e8a84ba76372210e29b545945522eee7ee4c84
franck <carnet.franck.paul@gmail.com>
2023-05-01 08:39:02 +0200

    l10n

---
+++ locales/en/main.lang.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package Dotclear
+ *
+ * @copyright Olivier Meunier & Association Dotclear
+ * @copyright GPL-2.0-only
+ */
+#
+# DOT NOT MODIFY THIS FILE !
+#
+
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Add user defined thumbnails'] = '';
+L10n::$locales['User defined thumbnails'] = '';
+L10n::$locales['Activate user defined thumbnails for this blog'] = '';
+L10n::$locales['Thumbnails sizes'] = '';
+L10n::$locales['Size in pixels'] = '';
+L10n::$locales['Mode'] = '';
+L10n::$locales['Clear any field in row to delete this row'] = '';
+L10n::$locales['Code must not be one of these: %s'] = '';
+L10n::$locales['Mode: <strong>ratio</strong> will preserve aspect, <strong>crop</strong> will produce square'] = '';
+L10n::$locales['Save'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -9,12 +9,14 @@
 # DOT NOT MODIFY THIS FILE !
 #
 
-l10n::$locales['Add user defined thumbnails'] = 'Ajoute des tailles de miniatures supplémentaires';
-l10n::$locales['User defined thumbnails'] = 'Miniatures supplémentaires';
-l10n::$locales['Activate user defined thumbnails for this blog'] = 'Activer les tailles de miniatures supplémentaires pour ce blog';
-l10n::$locales['Thumbnails sizes'] = 'Tailles supplémentaires de miniature';
-l10n::$locales['Size in pixels'] = 'Taille en pixels';
-l10n::$locales['Mode'] = 'Mode';
-l10n::$locales['Clear any field in row to delete this row'] = 'Videz n\'importe quel champs d\'une ligne pour supprimer la ligne';
-l10n::$locales['Code must not be one of these: %s'] = 'Le code ne doit pas être un de ceux-ci : %s';
-l10n::$locales['Mode: <strong>ratio</strong> will preserve aspect, <strong>crop</strong> will produce square'] = 'Mode : <strong>ratio</strong> conservera l\'aspect de l\'image, <strong>crop</strong> produira une image carrée';
+use Dotclear\Helper\L10n;
+
+L10n::$locales['Add user defined thumbnails'] = 'Ajoute des tailles de miniatures supplémentaires';
+L10n::$locales['User defined thumbnails'] = 'Miniatures supplémentaires';
+L10n::$locales['Activate user defined thumbnails for this blog'] = 'Activer les tailles de miniatures supplémentaires pour ce blog';
+L10n::$locales['Thumbnails sizes'] = 'Tailles supplémentaires de miniature';
+L10n::$locales['Size in pixels'] = 'Taille en pixels';
+L10n::$locales['Mode'] = 'Mode';
+L10n::$locales['Clear any field in row to delete this row'] = 'Videz n\'importe quel champs d\'une ligne pour supprimer la ligne';
+L10n::$locales['Code must not be one of these: %s'] = 'Le code ne doit pas être un de ceux-ci : %s';
+L10n::$locales['Mode: <strong>ratio</strong> will preserve aspect, <strong>crop</strong> will produce square'] = 'Mode : <strong>ratio</strong> conservera l\'aspect de l\'image, <strong>crop</strong> produira une image carrée';
