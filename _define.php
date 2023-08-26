df7ba4569699b121d5d4ad76c25619c1f7a67e3f
franck <carnet.franck.paul@gmail.com>
2023-08-26 12:34:37 +0200

    New release 3.0

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'User defined thumbnails',
     'Add user defined thumbnails',
     'Franck Paul',
-    '2.5.1',
+    '3.0',
     [
         'requires'    => [['core', '2.27'], ['php', '8.1']],
         'permissions' => dcCore::app()->auth->makePermissions([
