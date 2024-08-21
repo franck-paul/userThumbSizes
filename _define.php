c76d09f410943a1199e72363625c1366a46bb18e
franck <carnet.franck.paul@gmail.com>
2024-08-21 09:24:41 +0200

    New release 4.2

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'User defined thumbnails',
     'Add user defined thumbnails',
     'Franck Paul',
-    '4.1',
+    '4.2',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
